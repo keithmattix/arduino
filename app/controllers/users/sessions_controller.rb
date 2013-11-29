7ece4de93767f67acf54780f1919bdaf95e3c8a9
Keith Mattix <clear2codeu@gmail.com>
2013-11-29 17:21:55 -0600

    skip_before_filter authenticity

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,6 +4,9 @@ class SessionsController < Devise::SessionsController
     set_flash_message(:notice, :signed_in) if is_flashing_format?
     sign_in(resource_name, resource)
     yield resource if block_given?
-    respond_with resource, :location => user_projects_path
+    respond_to do |format|
+    	format.html {respond_with resource, :location => after_sign_in_path_for(resource)}
+    	format.json {respond_with resource}
+    end
 	end
 end
