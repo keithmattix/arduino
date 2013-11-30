0e64159b8f640c3d9777a81fa0c437453e3edf22
Keith Mattix <clear2codeu@gmail.com>
2013-11-29 18:06:58 -0600

    Add json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   before_filter :configure_permitted_parameters, if: :devise_controller?
   respond_to :html, :json
-  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
+  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' ||  c.request.format == 'text/json'}
  
   protected
 
