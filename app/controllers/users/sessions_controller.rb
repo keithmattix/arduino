a056a21d38bd678ef86a15ca07aa433b893013ae
Keith Mattix <clear2codeu@gmail.com>
2013-11-29 18:01:22 -0600

    Add json

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,12 +1,12 @@
 class Users::SessionsController < Devise::SessionsController
 	def create
 		self.resource = warden.authenticate!(auth_options)
-    set_flash_message(:notice, :signed_in) if is_flashing_format?
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_to do |format|
-    	format.html {respond_with resource, :location => after_sign_in_path_for(resource)}
-    	format.json {respond_with resource}
-    end
+        set_flash_message(:notice, :signed_in) if is_flashing_format?
+        sign_in(resource_name, resource)
+        yield resource if block_given?
+        respond_to do |format|
+        	format.html {respond_with resource, :location => after_sign_in_path_for(resource)}
+        	format.json {respond_with resource.to_json}
+        end
 	end
 end
